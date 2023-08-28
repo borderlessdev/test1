4e124c56854297a83845e2afad0870276e0b0bc5
borderlessdev <borderlesspc1@gmail.com>
2023-08-28 17:54:35 -0300

    Descrição das alterações

--- server.js
+++ server.js
@@ -1,43 +1,77 @@
 import express from 'express';
-import fs from 'fs';
+import bodyParser from 'body-parser';
 import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
-import { TempleWallet } from '@temple-wallet/dapp';
+import { importKey } from '@taquito/signer';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+
+const CONTRACT_ADDRESS = 'KT1FVfi5SFiWay5eqSX7Pr37ztP23oTnRuHi'; // Endereço do contrato
+const NETWORK = 'florencenet'; // Rede de teste Florencenet
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
 
-app.use(express.json());
+const tezos = new TezosToolkit('https://florencenet.smartpy.io'); // URL da rede Florencenet
+
+const wallet = new BeaconWallet({
+  name: 'Your Wallet Name', // Nome da carteira
+  preferredNetwork: NETWORK,
+});
+
+async function mintNFT(name, description, imageUrl) {
+  try {
+    const activeAccount = await wallet.client.getActiveAccount();
+
+    if (!activeAccount) {
+      throw new Error('No active account found');
+    }
+
+    const tezosWithWallet = new TezosToolkit('https://florencenet.smartpy.io');
+    tezosWithWallet.setWalletProvider(wallet);
+
+    const metadata = {
+      name: name,
+      description: description,
+      image: imageUrl,
+    };
+
+    const metadataMap = MichelsonMap.fromLiteral({
+      '': Buffer.from(JSON.stringify(metadata)).toString('hex'),
+    });
+
+    const contract = await tezosWithWallet.contract.at(CONTRACT_ADDRESS);
+
+    const operation = await contract.methods.mint(metadataMap).send();
+
+    await operation.confirmation();
+
+    console.log('NFT minted successfully');
+  } catch (error) {
+    console.error('Error minting NFT:', error);
+    throw error; // Rethrow the error to be caught by the route handler
+  }
+}
 
 app.post('/mint-nft', async (req, res) => {
   try {
-    const wallet = new TempleWallet('Borderless');
-    await wallet.connect('edo2net'); // Conectando à rede de testes Ghostnet
+    const { name, description, imageUrl } = req.body;
 
-    const Tezos = new TezosToolkit();
-    Tezos.setWalletProvider(wallet);
+    await wallet.requestPermissions({
+      network: {
+        type: NETWORK,
+        rpcUrl: 'https://florencenet.smartpy.io', // URL da rede Florencenet
+      },
+    });
 
-    const contractAddress = 'KT1FVfi5SFiWay5eqSX7Pr37ztP23oTnRuHi'; // Substitua pelo endereço do contrato
+    await mintNFT(name, description, imageUrl);
 
-    const contract = await Tezos.contract.at(contractAddress);
-
-    // Lendo o objeto do arquivo JSON enviado pelo cliente
-    const jsonFile = req.body.file;
-    const tokenMetadata = MichelsonMap.fromLiteral(jsonFile);
-
-    const operation = await contract.methods.mint(tokenMetadata).send();
-    await operation.confirmation();
-
-    console.log('NFT minted successfully! Transaction hash:', operation.hash);
-
-    await wallet.disconnect();
-
-    res.status(200).json({ message: 'NFT minted successfully!', txHash: operation.hash });
+    res.status(200).json({ message: 'NFT minted successfully' });
   } catch (error) {
-    console.error('Erro ao criar o NFT:', error);
-    res.status(500).json({ error: 'Erro ao criar o NFT' });
+    console.error('Error in API route:', error);
+    res.status(500).json({ error: 'An error occurred' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
